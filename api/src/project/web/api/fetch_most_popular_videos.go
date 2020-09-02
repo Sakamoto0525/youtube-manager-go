6bdedd522dd1b3c4cc869e8d0450e5611bb190de
Sakamoto0525 <525masaya@gmail.com>
2020-09-02 15:53:40 +0900

    youtubeAPI 人気動画一覧取得APIを実装

--- api/src/project/web/api/fetch_most_popular_videos.go
+++ api/src/project/web/api/fetch_most_popular_videos.go
@@ -1,12 +1,33 @@
 package api
 
 import (
+	"context"
+
 	"github.com/labstack/echo"
+	"github.com/sirupsen/logrus"
 	"github.com/valyala/fasthttp"
+	"google.golang.org/api/option"
+	"google.golang.org/api/youtube/v3"
 )
 
 func FetchMostPoplarVideos() echo.HandlerFunc {
 	return func(c echo.Context) error {
-		return c.JSON(fasthttp.StatusOK, "Most Popular")
+		key := "AIzaSyCJNFuzmAGeAe5ZCXSqgo-el8ysXmnxxMQ"
+
+		ctx := context.Background()
+
+		yts, err := youtube.NewService(ctx, option.WithAPIKey(key))
+		if err != nil {
+			logrus.Fatalf("Error createing new Youtube service: %v", err)
+		}
+
+		call := yts.Videos.List([]string{"id", "snippet"}).Chart("mostPopular").MaxResults(3)
+
+		res, err := call.Do()
+		if err != nil {
+			logrus.Fatalf("Error calling Youtube API: %v", err)
+		}
+
+		return c.JSON(fasthttp.StatusOK, res)
 	}
 }
