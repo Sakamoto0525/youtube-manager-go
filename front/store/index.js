981c4479873a0df74310b227b86e0a7c675cd03e
Sakamoto0525 <525masaya@gmail.com>
2020-09-14 07:30:50 +0900

    Video 詳細画面　作成

--- front/store/index.js
+++ front/store/index.js
@@ -2,14 +2,29 @@ import { createRequestClient } from '~/store/request-client';
 
 export const state = () => ({
   items: [],
+  item: [],
   meta: {},
 })
 
 export const actions = {
+  /**
+   * Video一覧を取得
+   */
   async fetchPopularVideos({ commit }, payload) {
     const client = createRequestClient(this.$axios)
     const res = await client.get(payload.uri, payload.params)
     commit('mutatePopularVideos', res)
+  },
+  /**
+   * Video詳細を取得
+   */
+  async findVideo({ commit }, payload) {
+    const client = createRequestClient(this.$axios)
+    const res = await client.get(payload.uri)
+    const params = {
+      ...res.video_list
+    }
+    commit('mutateVideo', params)
   }
 }
 
@@ -17,6 +32,11 @@ export const mutations = {
   mutatePopularVideos(state, payload) {
     state.items = payload.items ? state.items.concat(payload.items) : []
     state.meta = payload
+  },
+  mutateVideo(state, payload) {
+    console.log('きてるよ！')
+    const params = (payload.items && payload.items.length > 0) ? payload.items[0] : {}
+    state.item = params
   }
 }
 
@@ -26,5 +46,8 @@ export const getters = {
   },
   getMeta(state) {
     return state.meta
+  },
+  getVideo(state) {
+    return state.item
   }
 }
